27620c2c04ecea64774b80841d39fcec4ec594be
Ary Lopes <arysouzalopes@gmail.com>
2020-08-22 13:37:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container, Globals, CssBox, ObjectInput, Button } from './styles';
+
+
+export default function App() {
+    const [width, setWidth] = useState('200px');
+    const [height, setHeight] = useState('150px');
+    const [topLeft, setTopLeft] = useState('10px');
+    const [topRight, setTopRight] = useState('10px');
+    const [bottomLeft, setBottomLeft] = useState('10px');
+    const [bottomRight, setBottomRight] = useState('10px');
+    const [background, setBackground] = useState('#120202');
+    const [alertText, setAlertText] = useState('');
+
+    const boxStyle = {
+        width: width,
+        height: height,
+        borderRadius: `${topLeft} ${topRight} ${bottomLeft} ${bottomRight}`,
+        background: background,
+    } 
+
+    const copyText = () => {
+        const copyText = `
+            width: ${width};
+            height: ${height};
+            border-radius: ${topLeft} ${topRight} ${bottomLeft} ${bottomRight};
+            background: ${background};
+        `;
+        navigator.clipboard.writeText(copyText);
+        setAlertText("CSS code copied to clipboard!!");
+    }
+
+    if (alertText !== '') {
+        setTimeout(()=> {
+            setAlertText('');
+        }, 2000)
+    }
+
+    return (
+        <>
+            <Globals></Globals>
+            <Container>
+            <h1>Welcome to Border-radius Previewer App!</h1>
+                <div style={boxStyle}></div> 
+                <p>{alertText}</p>
+                <CssBox>
+                    <div>
+                        width:                            
+                        <ObjectInput 
+                            onChange={e => setWidth(e.target.value)}
+                            value={width}
+                        />;
+                    </div>
+                    <div>
+                        height:                            
+                        <ObjectInput 
+                            onChange={e => setHeight(e.target.value)}
+                            value={height}
+                        />;
+                    </div>
+                    <div>
+                        border-radius:                            
+                        <ObjectInput 
+                            onChange={e => setTopLeft(e.target.value)}
+                            value={topLeft}
+                        />
+                        <ObjectInput 
+                            onChange={e => setTopRight(e.target.value)}
+                            value={topRight}
+                        />
+                        <ObjectInput 
+                            onChange={e => setBottomLeft(e.target.value)}
+                            value={bottomLeft}
+                        />
+                        <ObjectInput 
+                            onChange={e => setBottomRight(e.target.value)}
+                            value={bottomRight}
+                        />
+                        ;
+                    </div>
+                    <div>
+                        background:                            
+                        <ObjectInput 
+                            onChange={e => setBackground(e.target.value)}
+                            value={background}
+                        />;
+                    </div>
+                    <Button onClick={copyText}>Copy CSS Result</Button>
+                </CssBox>
+            </Container>
+        </>
+    )
 }
 
-export default App;
---
+++ src/styles.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+export const Globals = createGlobalStyle `
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+
+    #root {
+        height: 100%;
+        width: 100vw;
+    }
+
+    body {
+        font: 400 1rem Fira Code;
+        line-height: 1.5;
+        color: #DF1619;
+        background-color: #f2eded;
+    }
+
+    h1 {
+        font-family: Do Hyeon;
+        margin: 1rem 0;
+    }
+`;
+
+export const Container = styled.div `
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    margin: 0 auto;
+    position: relative;
+
+    p {
+        position: absolute;
+        top: 60%;
+        font: 400 1.125em Do Hyeon;
+    }
+`;
+
+export const CssBox = styled.div `
+    margin-left: 2rem;
+    margin-bottom: 2rem;
+`;
+
+export const ObjectInput = styled.input `
+    width: 80px;
+    text-align: center;
+    margin-left: 0.5rem;
+    border-radius: 2px;
+    border: none;
+    background-color: #B6CCCE;
+    color: #353535;
+    font: 400 1rem Fira Code;
+`;
+
+export const Button = styled.button `
+    cursor: pointer;
+    width: 100%;
+    border: none;
+    background-color: #DF1619;
+    border-radius: 5px;
+    padding: 10px 0;
+    margin-top: 1rem;
+    color: #f2eded;
+    font: 400 1.125rem Do Hyeon;
+    transition: .2s;
+
+    &:hover {
+        background-color: #eb3336;
+    }
+`;
