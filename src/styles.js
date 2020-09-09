26ead1e6a3285255a8d9ace6286a84dabed653c0
Ary Lopes <ary.s.lopes22@gmail.com>
2020-09-09 17:09:46 -0300

    button update

--- src/styles.js
+++ src/styles.js
@@ -59,21 +59,3 @@ export const ObjectInput = styled.input `
     color: #353535;
     font: 400 1rem Fira Code;
 `;
-
-export const Button = styled.button `
-    cursor: pointer;
-    width: 100%;
-    border: none;
-    background: linear-gradient(-90deg, #df1619 50%, #4a6c6f 100%);
-    background-size: 200% auto;
-    border-radius: 5px;
-    padding: 10px 0;
-    margin-top: 1rem;
-    color: #f2eded;
-    font: 400 1.125rem Do Hyeon;
-    transition: .2s;
-
-    &:hover {
-        background-position: right center;
-    }
-`;
